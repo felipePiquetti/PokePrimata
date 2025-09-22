5fc8add15ff33f3119da8b3889891451c4af6d7c
felipePiquetti <piquetti6@gmail.com>
2025-09-22 03:43:06 -0300

    Entrega Final PM2 - PokePrimata

---
+++ src/data.js
@@ -0,0 +1,94 @@
+const pokedex = [
+  {
+    id: 1,
+    nome: "Bulbasaur",
+    habilidade: "Overgrow",
+    peso: 6.9,
+    altura: 0.7,
+    imagem: "./images/bulbasaur.png",
+    evolucao: "Ivysaur"
+  },
+  {
+    id: 2,
+    nome: "Ivysaur",
+    habilidade: "Overgrow",
+    peso: 13.0,
+    altura: 1.0,
+    imagem: "./images/ivysaur.png",
+    evolucao: "Venusaur"
+  },
+  {
+    id: 25,
+    nome: "Pikachu",
+    habilidade: "Static",
+    peso: 6.0,
+    altura: 0.4,
+    imagem: "./images/pikachu.png",
+    evolucao: "Raichu"
+  },
+  {
+    id: 26,
+    nome: "Raichu",
+    habilidade: "Static",
+    peso: 30.0,
+    altura: 0.8,
+    imagem: "./images/raichu.png",
+    evolucao: null
+  },
+  {
+    id: 52,
+    nome: "Meowth",
+    habilidade: "Pickup",
+    peso: 4.2,
+    altura: 0.4,
+    imagem: "./images/meowth.png",
+    evolucao: "Persian"
+  },
+  {
+    id: 133,
+    nome: "Eevee",
+    habilidade: "Run Away",
+    peso: 6.5,
+    altura: 0.3,
+    imagem: "./images/eevee.png",
+    evolucao: "Vaporeon / Jolteon / Flareon / etc"
+  },
+  {
+    id: 39,
+    nome: "Jigglypuff",
+    habilidade: "Cute Charm",
+    peso: 5.5,
+    altura: 0.5,
+    imagem: "./images/jigglypuff.png",
+    evolucao: "Wigglytuff"
+  },
+  {
+    id: 7,
+    nome: "Squirtle",
+    habilidade: "Torrent",
+    peso: 9.0,
+    altura: 0.5,
+    imagem: "./images/squirtle.png",
+    evolucao: "Wartortle"
+  },
+  {
+    id: 4,
+    nome: "Charmander",
+    habilidade: "Blaze",
+    peso: 8.5,
+    altura: 0.6,
+    imagem: "./images/charmander.png",
+    evolucao: "Charmeleon"
+  },
+  {
+    id: 150,
+    nome: "Mewtwo",
+    habilidade: "Pressure",
+    peso: 122.0,
+    altura: 2.0,
+    imagem: "./images/mewtwo.png",
+    evolucao: null
+  }
+];
+
+module.exports = pokedex;
